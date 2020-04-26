4f130ad3138e0f51f1ea7457fcd6d0660875cffa
Luca Tavernini <lucatavernini@lucas-macbook-pro.home>
2020-04-26 15:41:06 +0200

    Add non root user version of customer-core

--- build.sh
+++ build.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 echo "----- Creating eureka server -----"
-oc new-app https://github.com/Tavernoshca/LakesideMutual \
+oc new-app . \
   --context-dir=eureka-server \
   --strategy=docker \
   --name=eureka-server \
   -l app=lakeside-mutual
 
 echo "----- Creating spring boot admin -----"
-oc new-app https://github.com/Tavernoshca/LakesideMutual \
+oc new-app . \
   --context-dir=spring-boot-admin \
   --strategy=docker \
   --name=spring-boot-admin \
   -l app=lakeside-mutual \
-  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka
+  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka \
   -e SPRING_PROFILES_ACTIVE=eureka
 
 echo "----- Creating customer core -----"
-oc new-app https://github.com/Tavernoshca/LakesideMutual \
+oc new-app . \
   --context-dir=customer-core \
   --strategy=docker \
   --name=customer-core \
   -l app=lakeside-mutual \
-  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka
+  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka \
   -e SPRING_PROFILES_ACTIVE=eureka
 
 echo "----- Creating customer management backend -----"
-oc new-app https://github.com/Tavernoshca/LakesideMutual \
+oc new-app . \
   --context-dir=customer-management-backend \
   --strategy=docker \
   --name=customer-management-backend \
   -l app=lakeside-mutual \
-  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka
+  -e EUREKA_CLIENT_SERVICEURL_DEFAULTZONE=http://eureka-server:8761/eureka \
   -e SPRING_PROFILES_ACTIVE=eureka
 
 echo "----- Creating customer management frontend -----" 
-oc new-app https://github.com/Tavernoshca/LakesideMutual \
+oc new-app . \
   --context-dir=customer-management-frontend \
   --strategy=docker \
   --name=customer-management-frontend \
